17642c64e5e3ed2a2be857e686cc4dfbc0bdf55f
shams098 <shams@mawaqit.net>
2024-11-15 14:48:25 +0500

    handle the null value of time in getPrayers method

--- lib/prayer_services.dart
+++ lib/prayer_services.dart
@@ -33,7 +33,8 @@ class PrayerService {
     List<NotificationInfoModel> prayersList = [];
     var scheduledCount = 0;
     for (var key in prayerKeys) {
-      var obj = await PrayerNotificationService().getPrayerNotificationFromDB(key);
+      var obj =
+      await PrayerNotificationService().getPrayerNotificationFromDB(key);
       if (check(obj)) {
         scheduledCount++;
       }
@@ -47,39 +48,52 @@ class PrayerService {
     while (prayersList.length < (Platform.isIOS ? 70 : 10)) {
       i++;
       for (var key in prayerKeys) {
-        var obj = await PrayerNotificationService().getPrayerNotificationFromDB(key);
+        var obj =
+        await PrayerNotificationService().getPrayerNotificationFromDB(key);
         var index = prayerKeys.indexOf(key);
         if (check(obj)) {
           final mosque = await getMosque(obj.mosqueUuid ?? '');
 
-          var time = getPrayerTime(mosque, key, time: DateTime.now().add(Duration(days: i)));
-          var notificationData = await getPrayerDataByIndex(mosque, index);
-          var prayerName = _getPrayerName(index);
+          var time = getPrayerTime(mosque, key,
+              time: DateTime.now().add(Duration(days: i)));
+          if (time != null) {
+            var notificationData = await getPrayerDataByIndex(mosque, index);
+            var prayerName = _getPrayerName(index);
 
-          String indexStr = index.toString(), dayStr = time!.day.toString(), monthStr = time.month.toString();
-          String str = indexStr + dayStr + monthStr;
-          int alarmId = int.parse(str);
-          print('Alarm ID: $alarmId');
+            String indexStr = index.toString(),
+                dayStr = time.day.toString(),
+                monthStr = time.month.toString();
+            String str = indexStr + dayStr + monthStr;
+            int alarmId = int.parse(str);
+            print('Alarm ID: $alarmId');
 
-          NotificationInfoModel prayer = NotificationInfoModel(
-            mosqueName: mosque.name,
-            sound: obj.notificationSound,
-            prayerName: prayerName,
-            time: time,
-            notificationBeforeAthan: notificationData?.notificationBeforeAthan ?? 0,
-            alarmId: alarmId,
-          );
-
-          prayersList.add(prayer);
+            NotificationInfoModel prayer = NotificationInfoModel(
+              mosqueName: mosque.name,
+              sound: obj.notificationSound,
+              prayerName: prayerName,
+              time: time,
+              notificationBeforeAthan:
+              notificationData?.notificationBeforeAthan ?? 0,
+              alarmId: alarmId,
+            );
+            prayersList.add(prayer);
+          }
         }
       }
     }
+
     prayersList.removeWhere((element) {
-      return element.time!.isBefore(DateTime.now());
+      return element.time == null || element.time!.isBefore(DateTime.now());
     });
-    prayersList.sort(
-      (a, b) => a.time!.millisecondsSinceEpoch.toString().compareTo(b.time!.millisecondsSinceEpoch.toString()),
-    );
+
+    prayersList.sort((a, b) {
+      if (a.time == null && b.time == null) return 0; // Both null, consider equal
+      if (a.time == null) return 1; // `a` is null, so `b` should come first
+      if (b.time == null) return -1; // `b` is null, so `a` should come first
+      return a.time!.millisecondsSinceEpoch
+          .compareTo(b.time!.millisecondsSinceEpoch);
+    });
+
     prayersList = prayersList.sublist(0, Platform.isIOS ? 63 : 5);
     return prayersList;
   }
